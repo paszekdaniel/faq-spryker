223bad99d3cba1de1bdc9668fd2e79b932b862f5
paszekdaniel <daniel.paszek@accenture.com>
2022-08-29 09:28:57 +0200

    phpdocs and comments added

--- src/Pyz/Zed/Faq/Business/FaqBusinessMapper.php
+++ src/Pyz/Zed/Faq/Business/FaqBusinessMapper.php
@@ -6,6 +6,11 @@ use Generated\Shared\Transfer\FaqQuestionTransfer;
 
 class FaqBusinessMapper
 {
+    /**
+     * @param FaqQuestionTransfer $questionTransfer
+     * @param string $language
+     * @return FaqQuestionTransfer
+     */
     public static function translateQuestion(FaqQuestionTransfer $questionTransfer, string $language): FaqQuestionTransfer {
         foreach ($questionTransfer->getTranslations() as $translation) {
             if($translation->getLanguage() === $language) {
